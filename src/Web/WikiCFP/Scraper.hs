73c4966c6592eae3463693517dd371851e157af6
Toshio Ito <debug.ito@gmail.com>
2016-05-04 22:04:07 +0900

    Scraper: Add signatures of scraping functions.
    Type is now hidden. it's enough to re-export types from Scraper
    module. Still, Type should be separate module because it does not
    depend on any scraping functionality.

--- src/Web/WikiCFP/Scraper.hs
+++ src/Web/WikiCFP/Scraper.hs
@@ -4,5 +4,22 @@
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 -- 
 module Web.WikiCFP.Scraper
-       () where
+       ( Event(..),
+         HTML,
+         scrapeConfEvents,
+         scrapeSearchEvents
+       ) where
 
+import Data.Text (Text)
+
+import Web.WikiCFP.Scraper.Type (Event(..))
+
+
+-- | TODO: should this be ByteString??
+type HTML = Text
+
+scrapeConfEvents :: HTML -> Either String [Event]
+scrapeConfEvents = undefined
+
+scrapeSearchEvents :: HTML -> Either String [Event]
+scrapeSearchEvents = undefined
