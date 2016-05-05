f5ef29fc0ed5a392d0ebf0d8a57d26178701cacb
Toshio Ito <debug.ito@gmail.com>
2016-05-05 17:52:55 +0900

    Scalpel: partially implement rowsToEvents.

--- src/Web/WikiCFP/Scraper/Scalpel.hs
+++ src/Web/WikiCFP/Scraper/Scalpel.hs
@@ -11,13 +11,14 @@ module Web.WikiCFP.Scraper.Scalpel
          searchRoot
        ) where
 
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative ((<$>), (<*>), (<|>))
 import Control.Monad (guard, forM_)
 import Data.Text (Text, pack)
+import Data.Time (Day)
 import Data.Attoparsec.Text (Parser, parseOnly, skipSpace, string, endOfInput)
-import Text.HTML.Scalpel (Scraper, (@:), (@=), chroot, text, texts, attr)
+import Text.HTML.Scalpel (Scraper, (@:), (@=), chroot, chroots, text, texts, attr)
 
-import Web.WikiCFP.Scraper.Type (Event)
+import Web.WikiCFP.Scraper.Type (Event(..), When(..))
 
 type ErrorMsg = String
 
@@ -32,12 +33,15 @@ searchRoot :: Scraper' [Event]
 searchRoot = undefined
 
 
+-- | Scrape events from a table. Use with the root at @\<table\>@ tag.
+eventsTable :: Scraper' [Event]
+eventsTable = undefined -- chroots "tr" (eventRow1 <|> eventRow2 <|> eventRowHeader)
+
 -- | Intermediate result of parsing under events \<table\>.
 data EventRow = EventRowHeader
               | EventRow1 Text Text Text -- ^ shortName, URL and longName
               | EventRow2 Text Text Text -- ^ when, where, deadlines
               deriving (Eq,Ord,Show)
-  
 
 -- | Scrape header row. Use with the root at @\<tr\>@ tag.
 eventRowHeader :: Scraper' EventRow
@@ -64,6 +68,30 @@ eventRow2 = do
   guard $ length tds == 3
   return $ EventRow2 (tds !! 0) (tds !! 1) (tds !! 2)
 
+rowsToEvents :: [EventRow] -> Either ErrorMsg [Event]
+rowsToEvents [] = return []
+rowsToEvents (r:rest) = case r of
+  EventRowHeader -> rowsToEvents_noHeader rest
+  _ -> Left ("The first row must be the header, but it was " ++ show r)
+
+rowsToEvents_noHeader :: [EventRow] -> Either ErrorMsg [Event]
+rowsToEvents_noHeader [] = return []
+rowsToEvents_noHeader ((EventRow1 sn url ln) : (EventRow2 when wher dl) : rest) =
+  (:) <$> createEvent sn url ln when wher dl <*> rowsToEvents_noHeader rest
+rowsToEvents_noHeader rows = Left ("Error while parsing rows: " ++ show rows)
+
+createEvent :: Text -> Text -> Text -> Text -> Text -> Text -> Either ErrorMsg Event
+createEvent sname url lname when wher dl = do
+  when' <- parseWhen when
+  wher' <- parseWhere wher
+  dl' <- parseDeadlines dl
+  return Event { eventShortName = sname,
+                 eventURL = url,
+                 eventLongName = lname,
+                 eventWhen = when',
+                 eventWhere = wher',
+                 eventDeadlines = dl'
+               }
 
 -- * Parsers
 
@@ -73,3 +101,11 @@ parsable p t = either (const False) (const True) $ parseOnly p t
 spacedText :: Text -> Parser Text
 spacedText expected = skipSpace *> string expected <* skipSpace <* endOfInput
 
+parseWhen :: Text -> Either ErrorMsg (Maybe When)
+parseWhen = undefined
+
+parseWhere :: Text -> Either ErrorMsg (Maybe Text)
+parseWhere = undefined
+
+parseDeadlines :: Text -> Either ErrorMsg [Day]
+parseDeadlines = undefined
