ee737c35f62cfa4ec848d5a8502d908df477eb58
Toshio Ito <debug.ito@gmail.com>
2016-05-05 10:40:22 +0900

    Scraper: input HTML is now polymorphic.
    it should be Text (decoded), but we also accept ByteString for user's
    sake.

--- src/Web/WikiCFP/Scraper.hs
+++ src/Web/WikiCFP/Scraper.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 -- |
 -- Module: Web.WikiCFP.Scraper
 -- Description: Scrape WikiCFP web site
@@ -5,25 +6,48 @@
 -- 
 module Web.WikiCFP.Scraper
        ( Event(..),
-         HTML,
+         HTML(..),
          scrapeConfEvents,
          scrapeSearchEvents
        ) where
 
-import Data.Text (Text)
+import qualified Data.ByteString as SB
+import qualified Data.ByteString.Lazy as LB
+import Data.Text (Text, pack)
+import Data.Text.Encoding (decodeUtf8')
+import qualified Data.Text.Lazy as LT
 
 import Web.WikiCFP.Scraper.Type (Event(..))
 
+type ErrorMsg = String
 
--- | The input HTML data to scrape.
-type HTML = Text
+-- | Types of input HTML data to scrape.
+class HTML a where
+  decodeToText :: a -> Either ErrorMsg Text
+
+instance HTML Text where
+  decodeToText = Right
+
+instance HTML LT.Text where
+  decodeToText = Right . LT.toStrict
+
+-- | It just assumes UTF-8 encoding.
+instance HTML SB.ByteString where
+  decodeToText = either (\e -> Left $ "UTF-8 decoding error: " ++ show e) Right . decodeUtf8'
+
+instance HTML LB.ByteString where
+  decodeToText = decodeToText . LB.toStrict
+
+instance HTML String where
+  decodeToText = Right . pack
+
 
 -- | Scrape a page of a conference, for example,
 -- http://wikicfp.com/cfp/program?id=2671
-scrapeConfEvents :: HTML -> Either String [Event]
+scrapeConfEvents :: HTML input => input -> Either ErrorMsg [Event]
 scrapeConfEvents = undefined
 
 -- | Scrape a page of search results, for example,
 -- http://wikicfp.com/cfp/servlet/tool.search?q=cloud&year=t
-scrapeSearchEvents :: HTML -> Either String [Event]
+scrapeSearchEvents :: HTML input => input -> Either ErrorMsg [Event]
 scrapeSearchEvents = undefined
